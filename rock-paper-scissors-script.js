d48e7ac81878bb5e7381c96f818962e7ef0f6243
Adrian <193972263+st-ade@users.noreply.github.com>
2025-02-26 22:20:33 +0100

    Set up human input. Add play function (compare human input to random computer selection). Add scoring.

--- rock-paper-scissors-script.js
+++ rock-paper-scissors-script.js
@@ -2,23 +2,38 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(computerChoice) {
-    let randomNum = Math.floor((Math.random() * 100));
-        if (randomNum % 5 === 0)
-            {computerChoice = "Rock"}
-        else if (randomNum % 2 === 0)
-            {computerChoice = "Paper"} 
+    let randomNum = Math.floor(Math.random() * 3);
+        if (randomNum === 0)
+            {computerChoice = "rock"}
+        else if (randomNum === 1)
+            {computerChoice = "paper"} 
         else 
-            {computerChoice = "Scissors"}
+            {computerChoice = "scissors"}
 
     return computerChoice
 }
 
 function getHumanChoice(humanChoice) {
-    humanChoice = prompt("Choose rock, paper or scissors");
-    
-    return humanChoice
+    humanChoice = prompt("Choose rock, paper or scissors")
+
+    return humanChoice.toLowerCase()
 }
 
 function playRound(humanChoice, computerChoice) {
-        
+        if (humanChoice === computerChoice)
+        {console.log(`Computer played ${computerChoice}. It's a tie!`)}
+        else if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "scissors" && computerChoice == "rock") || (humanChoice == "paper" && computerChoice == "scissors"))
+        {console.log(`Computer played ${computerChoice}. You lose, ${computerChoice} beats ${humanChoice}!`);
+        computerScore = computerScore + 1;}
+        else if ((humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "paper"))
+            {console.log(`Computer played ${computerChoice}. You win, ${humanChoice} beats ${computerChoice}!`);
+            humanScore = humanScore + 1;}
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore)
+console.log(computerScore)
